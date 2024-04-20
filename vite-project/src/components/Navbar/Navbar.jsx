c6f95159382c6572c2d398a2ccf36b1e1dcad268
ehtisham <izhann000@gmail.com>
2024-04-20 17:22:49 +0500

    pages/routes/bootstrap

--- vite-project/src/components/Navbar/Navbar.jsx
+++ vite-project/src/components/Navbar/Navbar.jsx
@@ -1,10 +1,20 @@
 /* eslint-disable no-unused-vars */
 import React from 'react'
 import './Navbar.css'
+import { Link } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div>
+        <ul className='list-group list-group-horizontal'>
+        <li className='list-group-item'><Link to="/">Home</Link></li>
+        <li className='list-group-item'><Link to="/cart">Cart</Link></li>
+        </ul>
+    </div>
   )
 }
 
