30b5dab71d920480cef9a00913afad62249267b3
Izhan <57583461+1zhanN@users.noreply.github.com>
2024-04-21 21:42:43 +0500

    uploaded assets and rendered them

--- vite-project/src/pages/Home/Home.jsx
+++ vite-project/src/pages/Home/Home.jsx
@@ -1,10 +1,24 @@
 /* eslint-disable no-unused-vars */
-import React from 'react'
+import React from "react";
+import { assets, food_list } from "../../assets/assets";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div>
+      <h1>Food List</h1>
+      <div>
+        {food_list.map((item) => (
+          <div key={item._id}>
+            <img src={item.image} alt={item.name} />
+            <h3>{item.name}</h3>
+            <p>{item.description}</p>
+            <p>Price: ${item.price}</p>
+            <p>Category: {item.category}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
