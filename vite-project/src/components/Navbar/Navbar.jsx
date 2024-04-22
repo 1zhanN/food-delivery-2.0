1e4f133cfbdc6e8ea1c4ceceb037e5716efd0c7b
Izhan <57583461+1zhanN@users.noreply.github.com>
2024-04-22 14:16:10 +0500

    some styling

--- vite-project/src/components/Navbar/Navbar.jsx
+++ vite-project/src/components/Navbar/Navbar.jsx
@@ -1,21 +1,33 @@
 /* eslint-disable no-unused-vars */
-import React from 'react'
-import './Navbar.css'
-import { Link } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-
-
+import React from "react";
+import "./Navbar.css";
+import { Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { assets } from "../../assets/assets";
 
 const Navbar = () => {
   return (
     <div>
-        <ul className='list-group list-group-horizontal'>
-        <li className='list-group-item'><Link to="/">Home</Link></li>
-        <li className='list-group-item'><Link to="/cart">Cart</Link></li>
-        </ul>
+      <ul
+        className="list-group list-group-horizontal"
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
+        <li className="list-group-item">
+          <Link to="/">Home</Link>
+        </li>
+        <li className="list-group-item">
+          <Link to="/cart">Cart</Link>
+        </li>
+        <li style={{ marginLeft: "auto", padding: "15px" }}>
+          <img src={assets.basket_icon} alt="Basket Icon" />
+        </li>
+      </ul>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
